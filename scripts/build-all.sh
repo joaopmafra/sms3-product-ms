8be5ed0ba307f44fb1ff62d95d5c7b2d0891c6ff
João Paulo Mafra <joaopmafra@gmail.com>
2025-02-22 09:43:59 -0300

    Check if curl is installed in the build-all.sh script

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -3,12 +3,17 @@
 # stop execution if a command fails
 set -e
 
+# curl is needed by sdkman
+if ! command -v curl &> /dev/null; then
+  echo "curl is not installed. Please install curl and try again."
+  exit 1
+fi
+
 # enable sdkman
 scripts/sdkman-setup.sh
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
 sdk env install
-sdk env
 
 cd product-service
 ./gradlew clean build
